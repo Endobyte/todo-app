9e779c011680ea6fd1ac475ce5ffaad96eb6de22
crl2 <crl2@fresnou.org>
2022-10-17 11:34:42 -0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', async function(req, res) {
+  const {sequelize} = require("../models/index");
+  const {QueryTypes} = require("sequelize");
+
+  let toDoItems = await sequelize.query('select * from todo', {type: QueryTypes.SELECT });
+  res.render('index', {toDoItems});
+});
+
+router.get('/add', function(req, res){
+  res.render('create_todo');
+})
+
+router.post('/add', async function(req, res){
+  const {sequelize} = require("../models/index");
+  const {QueryTypes} = require("sequelize");
+  await sequelize.query('insert into todo (description) values (:description)', {
+    type: QueryTypes.INSERT,
+    replacements: {
+      description: req.body.description
+    }
+  });
+  res.redirect('/');
+})
+
+module.exports = router;
